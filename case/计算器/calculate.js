41d1b40e580d32836962ed1e8240cdf276ec76fd
1914k <k_1914@163.com>
2020-03-16 00:30:10 +0800

    Site updated: 2020-03-16 00:30:01

--- case/计算器/calculate.js
+++ case/计算器/calculate.js
@@ -4,88 +4,130 @@ window.onload = function () {
   let arr = [];
   let oBtnItem = document.getElementsByClassName("btn-item");
 
-  // 计算规则
+// ************************计算规则处理  start******************************
 
-  // 计算加减乘除时
-  changeSign = (className,result) => {
-    console.log(className,result);
+  // 把加减乘除符号换成对应的 + - * %
+  processOperaton = (className) => {
     switch(className) {
       case "division":
-        result += '/';
-        // division(result);
-        break;
+        return '/';
       case "multi":
-        result += '*';
-        break;
+        return '*';
       case "sub":
-        result += '-';
-        break;
+        return '-';
       case "inc":
-        result += '+';
-        break;
+        return '+';
       default: 
         break;
     }
   }
 
-  // 计算规则
+  //处理 % 后接数字的函数
+  addMultiOperaton = (arr, i) => {
+    if(i > 1) {
+      if(arr[i-1].curClass == "percentage") {
+        return "*" + arr[i].curInn;
+      }
+      return arr[i].curInn;
+    }
+    return arr[i].curInn;
+  }
+
+  // 处理 % 后直接接小数点的情况
+  addMultiOperatonBeforePoint = (arr, i) => {
+    if (arr[i-1] && arr[i-1].curClass == "percentage") {
+      return  "*.";
+    }
+    return arr[i].curInn
+  }
+// ************************计算规则处理  end******************************
+
+// ************************点击事件规则处理  start******************************
+  // 点击加减乘除时
+  clickCal = (curObj, i, last) => {
+    if(arr.length === 0) {
+      return 
+    }
+    
+    curObj.curClassTwo = i.classList[2];
+    if (arr[last].curClass === "cal") {
+      arr.splice(last, 1, curObj);
+      return
+    }
+    arr.push(curObj);
+  }
+
+  // 每次点击 one 的内容设置
+  oneInner = () => {
+    one = "";
+      for( let i of arr) {
+        one += i.curInn; 
+      } 
+  }
+
+  // 使用 eval 时式子以运算符号结尾报错的处理
+  calAfter = (curClass) => {
+    try{
+      let result = eval(calculate(arr));
+
+      two = result ? result : "";
+
+      if (curClass == "eq") {
+        one = two;
+        arr = [];
+        arr.push({curClass: "num", curInn: one});
+        two = ""
+      }
+     
+      document.getElementById('one').innerHTML = one;
+      document.getElementById('two').innerHTML = two;
+    }catch(e){
+      // e 是错误信息
+      // console.log(e);
+      
+      document.getElementById('one').innerHTML = one;
+      document.getElementById('two').innerHTML = two;
+    }
+  } 
+
+
+// ************************点击事件规则处理  end******************************
+
+
+
+  // 计算
   function calculate(arr) {
     let result = ''
-    console.log(arr);
     
     for(let i in arr) { 
       switch(arr[i].curClass) {
         case "cal":
-          switch(arr[i].curClassTwo) {
-            case "division":
-              result += '/';
-              // division(result);
-              break;
-            case "multi":
-              result += '*';
-              break;
-            case "sub":
-              result += '-';
-              break;
-            case "inc":
-              result += '+';
-              break;
-            default: 
-              break;
-          }
-          // changeSign(arr[i].curClassTwo, result)
+          // 把加减乘除符号换成对应的 + - * %
+          let ope = processOperaton(arr[i].curClassTwo);
+          result += ope;
           break;
         case "percentage":
           result += "/100";
           break;
         case "num":
-          if(i > 1) {
-            if(arr[i-1].curClass == "percentage") {
-              result += "*" + arr[i].curInn;
-              break;
-            }
-            result += arr[i].curInn;
-            break;
-          }
-          result += arr[i].curInn;
+          // 处理 % 后直接接数字的情况
+          let amo = addMultiOperaton(arr, i);
+          result += amo;
           break;
         case "point":
-          if (arr[i-1] && arr[i-1].curClass == "percentage") {
-            result += "*.";
-            break;
-          }
-          console.log("这里");
-          result += arr[i].curInn;
+          // 处理 % 后直接接小数点的情况
+          let amobp = addMultiOperatonBeforePoint(arr, i);
+          result += amobp;
           break;
         default:
           result += arr[i].curInn;
           break; 
       }
     }
-    console.log(result);
     return result;
   }
-  // console.log(78/9* 5/100* 2.6);
+
+  // 点击事件
   for (let i of oBtnItem) {
     i.onclick = function() {
       let curClass = this.classList[1];
@@ -95,12 +137,10 @@ window.onload = function () {
         curInn
       };
       let last = arr.length - 1;
+
       switch (curClass) {
         case "num":
           arr.push(curObj);
-          break;
-        case "eq":
-          console.log(arr);
           break;
         case "box-cle":
           arr = [];
@@ -109,21 +149,18 @@ window.onload = function () {
           arr.pop();
           break;
         case "cal":
-          curObj.curClassTwo = this.classList[2];
-          if (arr[last].curClass === "cal") {
-            arr.splice(last, 1, curObj);
-            break;
-          }
-          arr.push(curObj);
-          // console.log(arr);
+          // 点击加减乘除时符号的处理
+          clickCal(curObj, i, last);
           break;
         case "percentage":
+          // % 在第一位会显示错误
           if (!arr[last]) {
             two = "错误";           
           }
           arr.push(curObj);
           break;
         case "point":
+          // 小数点在最后一位点击小数点不会再增加小数点
           if (arr.length > 1 && arr[last].curClass === "point")
             break;
           arr.push(curObj);
@@ -131,27 +168,10 @@ window.onload = function () {
         default:
           break;
       }
-      one = "";
-      for( let i of arr) {
-        one += i.curInn; 
-      } 
-      
-      try{
-        let result = eval(calculate(arr));
-        
-        two = result ? result : "";
-        document.getElementById('one').innerHTML = one;
-        document.getElementById('two').innerHTML = two;
-      }catch(e){
-        console.log(e);
-        
-        document.getElementById('one').innerHTML = one;
-        document.getElementById('two').innerHTML = two;
-      }
-      
-      // 
-      
+      // 点击时one内容的变化
+      oneInner()
+      // 运算符号结尾报错处理
+      calAfter(curClass);
     }
   }
-  
 }
